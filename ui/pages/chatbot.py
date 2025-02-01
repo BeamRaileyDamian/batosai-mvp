813e9e054a3cc39d7ec574d297f385370128b8ff
BeamRaileyDamian <bmdamian@up.edu.ph>
2025-02-01 15:43:38 +0800

    added chatbot page

---
+++ ui/pages/chatbot.py
@@ -0,0 +1,64 @@
+import time
+from utils import *
+import streamlit as st
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+def main():
+    setup("Chatbot")
+
+    if not firebase_admin._apps:
+        cred = credentials.Certificate("firebase_config.json")
+        firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    if "lect_ids" not in st.session_state: st.session_state.lect_ids = get_all_document_ids(db, "lect_scripts")
+
+    col1, col2 = st.columns([3, 1])  # Adjust the widths of columns
+
+    # Create the selectbox inside one of the columns to control its width
+    with col1:
+        st.title("Chatbot")
+    with col2: 
+        option = st.selectbox(
+        'Chatbot Scope',
+            ["General"] + st.session_state.lect_ids
+        )
+
+    # if 'retriever' not in st.session_state or 'groq_api_key' not in st.session_state:
+    #     st.session_state.retriever, st.session_state.groq_api_key = setup()
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # Accept user input
+    if prompt := st.chat_input("What is up?"):
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        #response = rag_pipeline(prompt, st.session_state.retriever, st.session_state.groq_api_key)
+        response = "Hello World"
+
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            messagePlaceholder = st.empty()
+            typedResponse = ""
+            if response:
+                for char in response: # added typing effect
+                    typedResponse += char
+                    messagePlaceholder.markdown(typedResponse)
+                    time.sleep(0.01)
+
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+if __name__ == "__main__":
+    main()
--- ui/utils.py
+++ ui/utils.py
@@ -4,6 +4,7 @@ def setup(tabname):
     st.set_page_config(layout="wide", page_icon="🤖", page_title=tabname)
     st.sidebar.page_link("Home.py", label="Home", icon="🏠")
     st.sidebar.page_link("pages/modules.py", label="Modules", icon="📚")
+    st.sidebar.page_link("pages/chatbot.py", label="Chatbot", icon="🗨️")
     st.sidebar.page_link("pages/admin.py", label="Admin Panel", icon="⚙️")
 
 def get_all_document_ids(db, collection_name):
