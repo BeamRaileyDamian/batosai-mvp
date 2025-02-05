0208afad1386a26bd54f69e0e135be0d73f74742
BeamRaileyDamian <bmdamian@up.edu.ph>
2025-02-06 07:31:40 +0800

    added changing of scope

--- ui/pages/chatbot.py
+++ ui/pages/chatbot.py
@@ -7,7 +7,11 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
-from retriever import rag_pipeline, retriever_setup
+from retriever import rag_pipeline, retriever_setup, create_retriever
+
+def change_scope():
+    new_collection = st.session_state.selected_lect_id
+    st.session_state.retriever = create_retriever(new_collection)
 
 def main():
     setup("Chatbot")
@@ -22,19 +26,22 @@ def main():
         st.warning("No modules available. Ask admin to create modules to proceed.")
         st.stop()
 
+    if "selected_lect_id" not in st.session_state:
+        st.session_state.selected_lect_id = st.session_state.lect_ids[0]
+
     if st.session_state.lect_ids and 'retriever' not in st.session_state or 'groq_api_key' not in st.session_state:
-        st.session_state.retriever, st.session_state.groq_api_key = retriever_setup(st.session_state.lect_ids[0])
+        st.session_state.retriever, st.session_state.groq_api_key = retriever_setup(st.session_state.selected_lect_id)
 
     col1, col2 = st.columns([3, 1])  # Adjust the widths of columns
 
-    # Create the selectbox inside one of the columns to control its width
     with col1:
         st.title("Chatbot")
     with col2: 
-        option = st.selectbox(
-        'Chatbot Scope',
-            st.session_state.lect_ids
-            # ["General"] + st.session_state.lect_ids
+        st.selectbox(
+            label="Chatbot Scope",
+            options=st.session_state.lect_ids,
+            key="selected_lect_id",
+            on_change=change_scope
         )
 
     # Initialize chat history
