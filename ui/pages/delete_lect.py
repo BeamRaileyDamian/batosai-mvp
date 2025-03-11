918363a3903bf3d3c013aa4989d383d7fbb129b9
BeamRaileyDamian <bmdamian@up.edu.ph>
2025-03-11 18:24:50 +0800

    delete pdfs from supabase

--- ui/pages/delete_lect.py
+++ ui/pages/delete_lect.py
@@ -2,8 +2,9 @@ from utils import *
 import firebase_admin
 import streamlit as st
 from firebase_admin import firestore
+from supabase import create_client
 
-def delete(collection_name, document_id):
+def delete_from_firebase(db, collection_name, document_id):
     try:
         doc_ref = db.collection(collection_name).document(document_id)
 
@@ -16,25 +17,52 @@ def delete(collection_name, document_id):
     except Exception as e:
         print(f"An error occurred: {e}")
         return False
+    
+def delete_supabase_folder(client, bucket_name, folder_name):
+    response = client.storage.from_(bucket_name).list(folder_name)
 
-if not firebase_admin._apps:
-    init_firebase()
-db = firestore.client()
+    if response:
+        file_paths = [f"{folder_name}{file['name']}" for file in response]
+        delete_response = client.storage.from_(bucket_name).remove(file_paths)
 
-setup("Delete a Lecture")
-st.title("Delete a Lecture")
+        if delete_response:
+            return True
+        else:
+            return False
+    else:
+        return False
 
-if st.session_state.lect_ids:
-    for id in st.session_state.lect_ids:
-        if st.button(id, key=id):
-            to_delete = id
-            result = delete("lect_scripts", id)
-            
-            if result:
-                st.session_state.lect_ids.remove(id)
-                st.rerun()
-                # st.success("Lecture Successfully Deleted")
-            else:
-                st.success("Lecture Deletion Failed")
-else:
-    st.info("No lectures to display.")
+def main():
+    if not firebase_admin._apps:
+        init_firebase()
+    db = firestore.client()
+
+    setup("Delete a Lecture")
+    st.title("Delete a Lecture")
+
+    # Initialize Supabase client
+    SUPABASE_URL = st.secrets["SUPABASE_URL"]
+    SUPABASE_KEY = st.secrets["SUPABASE_API_KEY"]
+    client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+    # Define bucket name and folder to delete
+    bucket_name = st.secrets["BUCKET_NAME"]
+    folder_name = st.secrets["BUCKET_FOLDER_PDF"]
+
+    if st.session_state.lect_ids:
+        for id in st.session_state.lect_ids:
+            if st.button(id, key=id):
+                result = delete_from_firebase(db, "lect_scripts", id)
+                result2 = delete_supabase_folder(client, bucket_name, f"{folder_name}/{id}/")
+                
+                if result and result2:
+                    st.session_state.lect_ids.remove(id)
+                    st.rerun()
+                    # st.success("Lecture Successfully Deleted")
+                else:
+                    st.success("Lecture Deletion Failed")
+    else:
+        st.info("No lectures to display.")
+
+if __name__ == "__main__":
+    main()
